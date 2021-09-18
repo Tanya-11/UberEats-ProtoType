3aa94153502fcf9c0ca2fcfbba162c0a7880467a
Tanya-11 <tanya.gupta@sjsu.edu>
2021-09-17 18:04:03 -0700

    DB conn

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import Axios from "axios";
 
 function App() {
+  const name = 'ellen';
+  const dob = '1998-09-09';
+  const email = 'ellen1@gmail.com';
+  const phoneNo = 6689989999;
+  const city = 'San Jose';
+  const state = 'California';
+  const country = 'United States';
+  const imageUrl = ' ';
+
+
+  const addCustomerData = () => {
+    Axios.post("http://localhost:3001/create", {
+      name: name,
+      dob: dob,
+      email: email,
+      phoneNo: phoneNo,
+      city: city,
+      state: state,
+      country: country,
+      imageUrl: imageUrl
+    }).then(()=>{
+      console.log("Success");
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div onClick  ={addCustomerData}>
+      <h1>Hi</h1>
     </div>
   );
 }
